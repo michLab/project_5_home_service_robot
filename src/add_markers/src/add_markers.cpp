ec53e81a2c35bfb01e5eb52b53d04031a4950206
Michal Labowski <michal.labowski@gmail.com>
2019-07-28 19:25:49 +0000

    feat: Add user functions to add_markers node

--- src/add_markers/src/add_markers.cpp
+++ src/add_markers/src/add_markers.cpp
@@ -1,6 +1,13 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 
+bool initialize_marker(visualization_msgs::Marker& marker_in);
+bool set_marker_position_and_orientation(visualization_msgs::Marker& marker_in,
+                                         const float pos_x, const float pos_y, const float pos_z,
+                                         const float or_x, const float or_y, const float or_z, const float or_w);
+
+bool set_marker_state(ros::Publisher& marker_pub, visualization_msgs::Marker& marker_in, uint8_t state_in, float duration_in);
+
 int main( int argc, char** argv )
 {
   ros::init(argc, argv, "add_markers");
@@ -15,87 +22,73 @@ int main( int argc, char** argv )
   
   MarkerData marker_in_pick_up_zone;
   MarkerData marker_in_drop_off_zone;
-  
-  marker_in_pick_up_zone.pos_x = -3;
-  marker_in_pick_up_zone.pos_y = 2;
-  marker_in_pick_up_zone.pos_z = 0.1;
-  marker_in_pick_up_zone.or_x = 0;
-  marker_in_pick_up_zone.or_y = 0;
-  marker_in_pick_up_zone.or_z = 0;
-  marker_in_pick_up_zone.or_w = 1.0;
-  
-  marker_in_drop_off_zone.pos_x = -4;
-  marker_in_drop_off_zone.pos_y = -4;
-  marker_in_drop_off_zone.pos_z = 0.1;
-  marker_in_drop_off_zone.or_x = 0;
-  marker_in_drop_off_zone.or_y = 0;
-  marker_in_drop_off_zone.or_z = 0;
-  marker_in_drop_off_zone.or_w = 1.0;
-  
+  marker_in_pick_up_zone = {-3, 2, 0.1, 0, 0, 0, 1.0};
+  marker_in_drop_off_zone = {-4, -4, 0.1, 0, 0, 0, 1.0};
 
   visualization_msgs::Marker marker;
-  // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-  marker.header.frame_id = "map";
-  // Set the namespace and id for this marker.  This serves to create a unique ID
-  // Any marker sent with the same namespace and id will overwrite the old one
-  marker.ns = "add_markers";
-  marker.id = 0;
-  // Set our initial shape type to be a cube
-  uint32_t shape = visualization_msgs::Marker::CUBE;
-  // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-  marker.type = shape;
-  // Set the scale of the marker -- 1x1x1 here means 1m on a side
-  marker.scale.x = 0.1;
-  marker.scale.y = 0.1;
-  marker.scale.z = 0.1;
-  // Set the color -- be sure to set alpha to something non-zero!
-  marker.color.r = 1.0f;
-  marker.color.g = 0.0f;
-  marker.color.b = 0.0f;
-  marker.color.a = 1.0;
-  // Set marker duration:
-  marker.lifetime = ros::Duration();
+  initialize_marker(marker);
   
   while (ros::ok())
   {
-
-    marker.header.stamp = ros::Time::now();
-
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-    marker.action = visualization_msgs::Marker::ADD;
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = marker_in_pick_up_zone.pos_x;
-    marker.pose.position.y = marker_in_pick_up_zone.pos_y;
-    marker.pose.position.z = marker_in_pick_up_zone.pos_z;
-    marker.pose.orientation.x = marker_in_pick_up_zone.or_x;
-    marker.pose.orientation.y = marker_in_pick_up_zone.or_y;
-    marker.pose.orientation.z = marker_in_pick_up_zone.or_z;
-    marker.pose.orientation.w = marker_in_pick_up_zone.or_w;
-    marker_pub.publish(marker);
-	ros::Duration(5.0).sleep();
+	// Show marker in pick-up zone
+    set_marker_position_and_orientation(marker, marker_in_pick_up_zone.pos_x, marker_in_pick_up_zone.pos_y,
+                                        marker_in_pick_up_zone.pos_z, marker_in_pick_up_zone.or_x, marker_in_pick_up_zone.or_y,
+                                        marker_in_pick_up_zone.or_z, marker_in_pick_up_zone.or_w);
+    set_marker_state(marker_pub, marker, visualization_msgs::Marker::ADD, 5.0);
     
-    marker.header.stamp = ros::Time::now();
-    marker.action = visualization_msgs::Marker::DELETE;
-    marker_pub.publish(marker);
-    ros::Duration(5.0).sleep();
+    // Hide marker in pick-up zone (is carried now):
+    set_marker_state(marker_pub, marker, visualization_msgs::Marker::DELETE, 5.0);
     
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = marker_in_drop_off_zone.pos_x;
-    marker.pose.position.y = marker_in_drop_off_zone.pos_y;
-    marker.pose.position.z = marker_in_drop_off_zone.pos_z;
-    marker.pose.orientation.x = marker_in_drop_off_zone.or_x;
-    marker.pose.orientation.y = marker_in_drop_off_zone.or_y;
-    marker.pose.orientation.z = marker_in_drop_off_zone.or_z;
-    marker.pose.orientation.w = marker_in_drop_off_zone.or_w;
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.header.stamp = ros::Time::now();
-    marker_pub.publish(marker);
-    ros::Duration(5.0).sleep();
+    // Show marker in drop-off zone
+    set_marker_position_and_orientation(marker, marker_in_drop_off_zone.pos_x, marker_in_drop_off_zone.pos_y,
+                                        marker_in_drop_off_zone.pos_z, marker_in_drop_off_zone.or_x, marker_in_drop_off_zone.or_y,
+                                        marker_in_drop_off_zone.or_z, marker_in_drop_off_zone.or_w);
+	set_marker_state(marker_pub, marker, visualization_msgs::Marker::ADD, 5.0);
     
-    marker.action = visualization_msgs::Marker::DELETE;
-    marker.header.stamp = ros::Time::now();
-    marker_pub.publish(marker);
-    ros::Duration(5.0).sleep();
-    
+    // Hide marker in drop-off zone:
+	set_marker_state(marker_pub, marker, visualization_msgs::Marker::DELETE, 5.0);
   }
+}
+
+bool initialize_marker(visualization_msgs::Marker& marker_in) {
+  // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+  marker_in.header.frame_id = "map";
+  // Set the namespace and id for this marker.  This serves to create a unique ID
+  // Any marker sent with the same namespace and id will overwrite the old one
+  marker_in.ns = "add_markers";
+  marker_in.id = 0;
+  // Set our initial shape type to be a cube
+  uint32_t shape = visualization_msgs::Marker::CUBE;
+  // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+  marker_in.type = shape;
+  // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  marker_in.scale.x = 0.1;
+  marker_in.scale.y = 0.1;
+  marker_in.scale.z = 0.1;
+  // Set the color -- be sure to set alpha to something non-zero!
+  marker_in.color.r = 1.0f;
+  marker_in.color.g = 0.0f;
+  marker_in.color.b = 0.0f;
+  marker_in.color.a = 1.0;
+  // Set marker duration:
+  marker_in.lifetime = ros::Duration();
+}
+
+bool set_marker_position_and_orientation(visualization_msgs::Marker& marker_in,
+                                         const float pos_x, const float pos_y, const float pos_z,
+                                         const float or_x, const float or_y, const float or_z, const float or_w) {
+	marker_in.pose.position.x = pos_x;
+    marker_in.pose.position.y = pos_y;
+    marker_in.pose.position.z = pos_z;
+    marker_in.pose.orientation.x = or_x;
+    marker_in.pose.orientation.y = or_y;
+    marker_in.pose.orientation.z = or_z;
+    marker_in.pose.orientation.w = or_w;
+}
+
+bool set_marker_state(ros::Publisher& marker_pub, visualization_msgs::Marker& marker_in, uint8_t state_in, float duration_in = 5.0) {
+	marker_in.header.stamp = ros::Time::now();
+    marker_in.action = state_in;
+    marker_pub.publish(marker_in);
+    ros::Duration(duration_in).sleep();
 }
